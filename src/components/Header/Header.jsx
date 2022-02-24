b12d964fbb3357838f9d1fd1bd00b2f92b6a4420
Emma Egstad <emma@egstad.com>
2022-02-24 10:41:22 -0800

    front end sign in working, updating header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,24 @@
 import './Header.css';
 import React from 'react';
+import { useUser } from '../../context/UserContext';
+import { useHistory } from 'react-router-dom';
 
 export default function Header() {
+  const { user } = useUser();
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/login');
+  };
+
   return (
     <div className="Header">
       <h1 className="header-title">Acme Employee Directory</h1>
-      {/* conditionally render 'not signed in' or 'signed in as...' */}
       <section className="header-controls">
-        <span>Not Signed In</span>
-        <button>Log In</button>
+        <span>
+          {user.email ? `Signed-in as ${user.email}` : 'Not Signed In'}
+        </span>
+        <button onClick={handleClick}>Log In</button>
       </section>
     </div>
   );
--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -1,8 +1,31 @@
 import './Auth.css';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { useUser } from '../../context/UserContext';
 
 export default function Auth({ isSigningUp = false }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { user, setUser } = useUser();
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (isSigningUp) {
+      setUser({
+        email: email,
+        password: password,
+      });
+    } else {
+      setUser({
+        email: email,
+        password: password,
+      });
+      history.replace('/');
+    }
+  };
+
   return (
     <form className="Auth">
       <h2 className="auth-title">
@@ -16,7 +39,11 @@ export default function Auth({ isSigningUp = false }) {
           type="email"
           id="email-input"
           name="email-input"
+          value={email}
           className="auth-input_email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
         />
       </label>
       <label htmlFor="password-input">
@@ -25,10 +52,16 @@ export default function Auth({ isSigningUp = false }) {
           type="password"
           id="password-input"
           name="password-input"
+          value={password}
           className="auth-input_password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
         />
       </label>
-      <button type="submit">Submit</button>
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
       {isSigningUp ? (
         <p>
           Already have an account? <Link to="/login">Log In</Link>
